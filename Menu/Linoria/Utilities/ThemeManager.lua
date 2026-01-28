52a525fd9af01e6d2912b2641dedb211e3c19cef
Li <164679412+computerintrusion@users.noreply.github.com>
2026-01-28 14:40:07 -0600

    Update ThemeManager.lua

--- Menu/Linoria/Utilities/ThemeManager.lua
+++ Menu/Linoria/Utilities/ThemeManager.lua
@@ -99,31 +99,31 @@ local ThemeManager = {} do
 			self:ApplyTheme(Options.ThemeManager_ThemeList.Value)
 		end)
 
-		groupbox:AddDivider()
-		groupbox:AddInput('ThemeManager_CustomThemeName', { Text = 'Custom theme name' })
-		groupbox:AddDropdown('ThemeManager_CustomThemeList', { Text = 'Custom themes', Values = self:ReloadCustomThemes(), AllowNull = true, Default = 1 })
-		groupbox:AddDivider()
+		-- groupbox:AddDivider()
+		-- groupbox:AddInput('ThemeManager_CustomThemeName', { Text = 'Custom theme name' })
+		-- groupbox:AddDropdown('ThemeManager_CustomThemeList', { Text = 'Custom themes', Values = self:ReloadCustomThemes(), AllowNull = true, Default = 1 })
+		-- groupbox:AddDivider()
 		
-		groupbox:AddButton('Save Theme', function() 
-			self:SaveCustomTheme(Options.ThemeManager_CustomThemeName.Value)
+		-- groupbox:AddButton('Save Theme', function() 
+		-- 	self:SaveCustomTheme(Options.ThemeManager_CustomThemeName.Value)
 
-			Options.ThemeManager_CustomThemeList:SetValues(self:ReloadCustomThemes())
-			Options.ThemeManager_CustomThemeList:SetValue(nil)
-		end):AddButton('Load Theme', function() 
-			self:ApplyTheme(Options.ThemeManager_CustomThemeList.Value) 
-		end)
+		-- 	Options.ThemeManager_CustomThemeList:SetValues(self:ReloadCustomThemes())
+		-- 	Options.ThemeManager_CustomThemeList:SetValue(nil)
+		-- end):AddButton('Load Theme', function() 
+		-- 	self:ApplyTheme(Options.ThemeManager_CustomThemeList.Value) 
+		-- end)
 
-		groupbox:AddButton('Refresh', function()
-			Options.ThemeManager_CustomThemeList:SetValues(self:ReloadCustomThemes())
-			Options.ThemeManager_CustomThemeList:SetValue(nil)
-		end)
+		-- groupbox:AddButton('Refresh', function()
+		-- 	Options.ThemeManager_CustomThemeList:SetValues(self:ReloadCustomThemes())
+		-- 	Options.ThemeManager_CustomThemeList:SetValue(nil)
+		-- end)
 
-		groupbox:AddButton('Set Default', function()
-			if Options.ThemeManager_CustomThemeList.Value ~= nil and Options.ThemeManager_CustomThemeList.Value ~= '' then
-				self:SaveDefault(Options.ThemeManager_CustomThemeList.Value)
-				self.Library:Notify(string.format('Set default theme to %q', Options.ThemeManager_CustomThemeList.Value))
-			end
-		end)
+		-- groupbox:AddButton('Set Default', function()
+		-- 	if Options.ThemeManager_CustomThemeList.Value ~= nil and Options.ThemeManager_CustomThemeList.Value ~= '' then
+		-- 		self:SaveDefault(Options.ThemeManager_CustomThemeList.Value)
+		-- 		self.Library:Notify(string.format('Set default theme to %q', Options.ThemeManager_CustomThemeList.Value))
+		-- 	end
+		-- end)
 
 		ThemeManager:LoadDefault()
 
